355dc03c461b9e9b72d731c6af3a271134466f09
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-01-25 17:21:43 +0530

    error handling for login

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -1,14 +1,91 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Center, Container, Square, Stack } from '@chakra-ui/react';
 import GoogleAuth from '../../components/googleAuth/GoogleAuth';
 import { loginUser, userActionReset } from '../../actions/userActions';
 import { connect } from 'react-redux';
+import {
+  LOGIN,
+  REGISTER,
+  AUTHERROR,
+  ERROR,
+  INVALID,
+} from '../../utils/constants';
+import { useHistory } from 'react-router-dom';
+import { useToast } from '@chakra-ui/react';
 import './LoginPage.css';
 
 const LoginPage = ({ userData, loginUser, userActionReset }) => {
+  const history = useHistory();
+  const toast = useToast();
+
   const loginRequest = (authData) => {
     loginUser(authData);
   };
+
+  const alert = (title, description, status) => {
+    toast({
+      title,
+      description,
+      status,
+      duration: 3000,
+      isClosable: false,
+    });
+  };
+
+  // Error handling
+  useEffect(() => {
+    if (
+      userData.loginData.error &&
+      userData.loginData.error.payload !== undefined
+    ) {
+      switch (userData.loginData.error.payload.message) {
+        case REGISTER:
+          alert(
+            'Error on Login',
+            'You will have to Sign Up first to use InterviewHut!',
+            'error'
+          );
+          userActionReset();
+          break;
+        case ERROR:
+          alert(
+            'Error on Login',
+            'Some error occurred, we are working to fix it',
+            'error'
+          );
+          userActionReset();
+          break;
+        case AUTHERROR:
+        case INVALID:
+          alert(
+            'Error on Login',
+            'Some error occurred, please try again later',
+            'error'
+          );
+          userActionReset();
+          break;
+        default:
+          alert(
+            'Error on Login',
+            'Some error occurred, please try again later',
+            'error'
+          );
+          userActionReset();
+          break;
+      }
+    } else if (userData.loginData.error) {
+      alert('Error on Login', userData.loginData.error, 'error');
+      userActionReset();
+    }
+  }, [userData.loginData.error, userActionReset]);
+
+  useEffect(() => {
+    if (userData.loginData.data) {
+      if (userData.loginData.data === LOGIN) {
+        history.push('/');
+      }
+    }
+  }, [userData.loginData.data, history]);
 
   return (
     <Container>
