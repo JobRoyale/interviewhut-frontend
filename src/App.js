764a94b7f317919a938229f051fbbf8d35ac8f14
Alan Henry <alanhenry0221@gmail.com>
2021-01-31 08:59:53 +0530

    added chakraProvider and fixed sizing issue

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import FrontPage from './pages/frontpage/FrontPage';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
     <BrowserRouter>
+      <ChakraProvider>
         <div>
-            <Switch>
-             <Route path="/" component={FrontPage}/>
-           </Switch>
-        </div> 
-      </BrowserRouter>
+          <Switch>
+            <Route path="/" component={FrontPage}/>
+          </Switch>
+        </div>
+      </ChakraProvider>
+    </BrowserRouter>
   );
 }
 
