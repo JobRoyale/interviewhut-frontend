af562ae6586b824d328f19d0ab67a1c18a60cd38
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-03-23 12:30:08 +0530

    Small changes

--- src/actions/types.js
+++ src/actions/types.js
@@ -8,11 +8,14 @@ export const SIGNUP_LOADING = 'SIGNUP_LOADING';
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_FAIL = 'SIGNUP_FAIL';
 
+// Room Actions
 export const CREATE_ROOM_SUCCESS = 'CREATE_ROOM_SUCCESS';
 export const CREATE_ROOM_FAIL = 'CREATE_ROOM_FAIL';
 
 export const JOIN_ROOM_SUCCESS = 'JOIN_ROOM_SUCCESS';
 export const JOIN_ROOM_FAIL = 'JOIN_ROOM_FAIL';
+
+export const ROOM_UPDATED = 'ROOM_UPDATED';
 
 //Lobby Action
 export const CONNECTION_FAILED = 'CONNECTION_FAILED';
