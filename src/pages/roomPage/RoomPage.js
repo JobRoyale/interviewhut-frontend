69ebd440e9bf36ce3d442590e5355d2410d21cfb
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-06 12:13:52 +0530

    Added room id copying funct

--- src/pages/roomPage/RoomPage.js
+++ src/pages/roomPage/RoomPage.js
@@ -1,5 +1,20 @@
-import React, { useEffect } from 'react';
-import { Button, Flex } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Flex,
+  Stack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useDisclosure,
+  Tooltip,
+} from '@chakra-ui/react';
 import RoomMember from '../../components/roomMember/RoomMember';
 import Header from '../../components/header/Header';
 import { connect } from 'react-redux';
@@ -18,9 +33,12 @@ const RoomPage = ({
   startInterview,
   interviewStarted,
 }) => {
+  const [copied, setCopied] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const history = useHistory();
   const socket = socketData.socket;
   const username = getUserData().username;
+  const roomId = roomData.room.config.id;
 
   let roomMemberCards = null;
   let roomMembers;
@@ -62,6 +80,17 @@ const RoomPage = ({
     startInterview(socket);
   };
 
+  const handleCopyRoomIdClick = () => {
+    // Copying roomCode to clipboard
+    navigator.clipboard.writeText(roomId);
+    setCopied(true);
+
+    // Stop showing copied tooltip after 2 seconds
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
   return (
     <div>
       <Header loggedIn={true} />
@@ -80,17 +109,49 @@ const RoomPage = ({
           <Flex>{roomMemberCards}</Flex>
           {socket ? (
             roomData.room.config.admin === username ? (
-              <Button
-                width="40%"
-                colorScheme="blue"
-                onClick={handleStartInterview}
-              >
-                Start Interview
-              </Button>
+              <Stack width="100%" justifyContent="center" alignItems="center">
+                <Button width="40%" colorScheme="blue" onClick={onOpen}>
+                  Get Room Code
+                </Button>
+                <Button
+                  width="40%"
+                  colorScheme="red"
+                  onClick={handleStartInterview}
+                >
+                  Start Interview
+                </Button>
+              </Stack>
             ) : null
           ) : null}
         </Flex>
         <RoomChat />
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Invite other participant with room code</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <InputGroup size="md">
+                <Input pr="4.5rem" value={roomId} readOnly />
+                <InputRightElement width="4.5rem">
+                  <Tooltip
+                    label="Copied"
+                    aria-label="A tooltip"
+                    isOpen={copied}
+                  >
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handleCopyRoomIdClick}
+                    >
+                      Copy
+                    </Button>
+                  </Tooltip>
+                </InputRightElement>
+              </InputGroup>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       </Flex>
     </div>
   );
