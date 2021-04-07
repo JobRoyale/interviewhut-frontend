bbdbf70585fb669fe78581e94dc86f62332eec40
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-08 00:48:19 +0530

    Env for lobb

--- src/actions/socketActions.js
+++ src/actions/socketActions.js
@@ -1,32 +1,30 @@
 import io from 'socket.io-client';
-import {CONNECTION_FAILED,CONNECTION_ACCEPTED} from '../actions/types';
+import { CONNECTION_FAILED, CONNECTION_ACCEPTED } from '../actions/types';
 
-export const connectSocket = () =>(dispatch)=>{
-    const options = {
-        transportOptions: {
-          polling: {
-            extraHeaders: {
-              Authorization: `Bearer ${localStorage.token}`,
-            },
-          },
+export const connectSocket = () => (dispatch) => {
+  const options = {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: `Bearer ${localStorage.token}`,
         },
-      };
+      },
+    },
+  };
 
-     let socket = io.connect("http://localhost:2500", options);
-    socket.on("CONNECTION_ACK", () => {
-      console.log(CONNECTION_ACCEPTED);
-      dispatch({
-        type: CONNECTION_ACCEPTED,
-        payload: socket
-      })
+  let socket = io.connect(process.env.REACT_APP_LOBBY, options);
+  socket.on('CONNECTION_ACK', () => {
+    console.log(CONNECTION_ACCEPTED);
+    dispatch({
+      type: CONNECTION_ACCEPTED,
+      payload: socket,
     });
-    socket.on("CONNECTION_DENY", (data) => {
-      console.log(CONNECTION_FAILED);
-      dispatch(
-        {
-          type: CONNECTION_FAILED,
-          payload: data,
-        }
-      )
+  });
+  socket.on('CONNECTION_DENY', (data) => {
+    console.log(CONNECTION_FAILED);
+    dispatch({
+      type: CONNECTION_FAILED,
+      payload: data,
     });
-}
+  });
+};
